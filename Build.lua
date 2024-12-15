001e3af9d562aebf3f9fe30f37c10ff123505870
adnankuran0 <prodarzyn@gmail.com>
2024-12-16 01:07:51 +0300

    Some costumization

--- Build.lua
+++ Build.lua
@@ -1,8 +1,8 @@
 -- premake5.lua
-workspace "New Project"
+workspace "Rvela"
    architecture "x64"
    configurations { "Debug", "Release", "Dist" }
-   startproject "App"
+   startproject "Editor"
 
    -- Workspace-wide build options for MSVC
    filter "system:windows"
@@ -10,8 +10,8 @@ workspace "New Project"
 
 OutputDir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}"
 
-group "Core"
-	include "Core/Build-Core.lua"
+group "Engine"
+	include "Core/Build-Engine.lua"
 group ""
 
-include "App/Build-App.lua"
+include "App/Build-Editor.lua"
