3a04f771ad424cdf1fbb0d594504a7b9aa894462
adnan_ <kuranadnan1@gmail.com>
2026-03-26 14:26:29 +0300

    Added default material

--- Engine/Source/Asset/Loaders/MaterialLoader.cpp
+++ Engine/Source/Asset/Loaders/MaterialLoader.cpp
@@ -9,6 +9,12 @@
 using namespace rv;
 using json = nlohmann::json;
 
+MaterialLoader::MaterialLoader()
+{
+    AssetUUID uuid{};
+    s_DefaultMaterial = CreateRef<MaterialAsset>(uuid);
+}
+
 Ref<Asset> MaterialLoader::Load(
     const std::filesystem::path& assetPath,
     const AssetMeta& meta)
@@ -33,12 +39,12 @@ Ref<Asset> MaterialLoader::Load(
 
     auto asset = CreateRef<MaterialAsset>(meta.uuid);
 
-    if (j.contains("albedoTexture")) { asset->albedoTextureUUID = AssetUUID::FromString(j["albedoTexture"]);    asset->useAlbedoMap = true; }
-    if (j.contains("normalTexture")) { asset->normalTextureUUID = AssetUUID::FromString(j["normalTexture"]);    asset->useNormalMap = true; }
-    if (j.contains("metallicTexture")) { asset->metallicTextureUUID = AssetUUID::FromString(j["metallicTexture"]);  asset->useMetallicMap = true; }
+    if (j.contains("albedoTexture")) { asset->albedoTextureUUID = AssetUUID::FromString(j["albedoTexture"]); asset->useAlbedoMap = true; }
+    if (j.contains("normalTexture")) { asset->normalTextureUUID = AssetUUID::FromString(j["normalTexture"]); asset->useNormalMap = true; }
+    if (j.contains("metallicTexture")) { asset->metallicTextureUUID = AssetUUID::FromString(j["metallicTexture"]); asset->useMetallicMap = true; }
     if (j.contains("roughnessTexture")) { asset->roughnessTextureUUID = AssetUUID::FromString(j["roughnessTexture"]); asset->useRoughnessMap = true; }
-    if (j.contains("aoTexture")) { asset->aoTextureUUID = AssetUUID::FromString(j["aoTexture"]);        asset->useAOMap = true; }
-    if (j.contains("heightTexture")) { asset->heightTextureUUID = AssetUUID::FromString(j["heightTexture"]);    asset->useHeightMap = true; }
+    if (j.contains("aoTexture")) { asset->aoTextureUUID = AssetUUID::FromString(j["aoTexture"]); asset->useAOMap = true; }
+    if (j.contains("heightTexture")) { asset->heightTextureUUID = AssetUUID::FromString(j["heightTexture"]); asset->useHeightMap = true; }
 
     if (j.contains("albedoColor")) asset->albedoColor = { j["albedoColor"][0], j["albedoColor"][1], j["albedoColor"][2] };
     if (j.contains("emissiveColor")) asset->emissiveColor = { j["emissiveColor"][0], j["emissiveColor"][1], j["emissiveColor"][2] };
